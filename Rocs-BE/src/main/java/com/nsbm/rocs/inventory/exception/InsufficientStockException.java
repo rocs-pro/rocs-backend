af51e4ab54c39cf128184052fde6ac60787a1246
VishwaChandeepa <chandeepavishwa@gmail.com>
2026-01-18 00:46:30 +0530

    feat(inventory): add inventory-specific exceptions and handlers

---
+++ Rocs-BE/src/main/java/com/nsbm/rocs/inventory/exception/InsufficientStockException.java
@@ -0,0 +1,8 @@
+package com.nsbm.rocs.inventory.exception;
+
+public class InsufficientStockException extends RuntimeException {
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
+
