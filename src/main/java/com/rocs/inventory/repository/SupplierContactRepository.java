c4cc46ee06fe80dd85106d959d78836ac457fa8a
VishwaChandeepa <chandeepavishwa@gmail.com>
2026-02-01 01:41:22 +0530

    Add supplier and inventory repositories

---
+++ src/main/java/com/rocs/inventory/repository/SupplierContactRepository.java
@@ -0,0 +1,8 @@
+package com.rocs.inventory.repository;
+
+import com.rocs.inventory.entity.SupplierContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierContactRepository extends JpaRepository<SupplierContact, Long> {
+}
+
